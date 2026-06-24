127d823cd30894ca4c43094c1a85dfd4bbd47a99
Prabhat kumar <prabhat844502@gmail.com>
2026-06-24 15:29:39 +0530

    Basics arrays Practice questions

---
+++ 01_Arrays/03.cpp
@@ -0,0 +1,16 @@
+//1. Reverse an array
+https://www.geeksforgeeks.org/problems/reverse-an-array/1
+
+//2. Check if array is sorted
+https://www.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1?utm_source=chatgpt.com
+
+//3. Largest array element
+https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
+
+//4. Second largest element in an array
+https://www.geeksforgeeks.org/problems/second-largest3735/1
+
+//5. Maximum consecutive one's
+https://www.naukri.com/code360/problems/maximum-consecutive-ones_3843993?leftPanelTabValue=SUBMISSION
+
+
---
+++ 01_Arrays/Practice.java
@@ -0,0 +1,21 @@
+public class Practice {
+    public static int linearSearch(int[] arr, int target) {
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 32, 55, 1, 26, 53, 78};
+        int target = 53;
+        int ans = linearSearch(arr, target);
+        if(ans == -1) {
+            System.out.println("Target not found");
+        } else {
+            System.out.println("Target found at index: " + ans);
+        }
+
+    }
+}
