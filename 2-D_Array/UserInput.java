815a78529e48e941569dbc9c26d7f496b325ba5a
Prabhat kumar <prabhat844502@gmail.com>
2026-07-03 13:12:11 +0530

    column wise 2-d array printing

--- 2-D_Array/UserInput.java
+++ 2-D_Array/UserInput.java
@@ -6,7 +6,7 @@ public class UserInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int arr[][] = new int[4][3];
+        int arr[][] = new int[3][3];
         //Taking input rowise
         // for(int i=0;i<3;i++) {
         //     for(int j=0;j<4;j++){
@@ -21,15 +21,17 @@ public class UserInput {
         // }
 
 
+        System.out.println("Write the elements of matrix: ");
         //Taking user input as column wise
-        for(int i=0;i<4;i++) {
-            for(int j=0;j<3;j++){
+        for(int i=0;i<arr.length;i++) {
+            for(int j=0;j<arr[i].length;j++){
                 arr[i][j]=sc.nextInt();
             }
         }
-        for(int i=0;i<4;i++) {
-            for(int j=0;j<3;j++){
-                System.out.print(arr[j][i]+" ");
+        //Printing column wise
+        for(int j=0;j<arr[0].length;j++) {
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
