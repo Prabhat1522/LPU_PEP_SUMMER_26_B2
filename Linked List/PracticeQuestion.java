9ccb57c11b87ad8c2bb38da4db083b56773ec9cd
Prabhat kumar <prabhat844502@gmail.com>
2026-07-07 23:50:32 +0530

    Insert in a sorted List

--- Linked List/PracticeQuestion.java
+++ Linked List/PracticeQuestion.java
@@ -4,6 +4,9 @@ https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
 // 2. Palindrome Linked List
 https://www.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1
 
+// 3. Insert in a sorted List
+https://www.geeksforgeeks.org/problems/insert-in-a-sorted-list/1
+
 
 
 public class PracticeQuestion {
