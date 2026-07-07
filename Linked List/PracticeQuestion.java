a61878950f6833510152e398223ad5a1f49acb21
Prabhat kumar <prabhat844502@gmail.com>
2026-07-07 23:58:45 +0530

    Detect Loops in Linked List cycles

--- Linked List/PracticeQuestion.java
+++ Linked List/PracticeQuestion.java
@@ -11,6 +11,13 @@ https://www.geeksforgeeks.org/problems/insert-in-a-sorted-list/1
 https://www.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
 https://leetcode.com/problems/intersection-of-two-linked-lists/description/
 
+// 5. 141. Linked List Cycle
+https://leetcode.com/problems/linked-list-cycle/description/
+https://www.geeksforgeeks.org/problems/detect-loop-in-linked-list/1
+
+// 6. 142. Linked List Cycle II
+https://leetcode.com/problems/linked-list-cycle-ii/description/
+
 
 
 public class PracticeQuestion {
