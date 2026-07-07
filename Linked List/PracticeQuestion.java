ba58f711175ea7723c9eb0d89bd830b164c6bb3a
Prabhat kumar <prabhat844502@gmail.com>
2026-07-07 23:48:53 +0530

    Palindrome LL

--- Linked List/PracticeQuestion.java
+++ Linked List/PracticeQuestion.java
@@ -1,6 +1,22 @@
 // 1 . Reverse Linked List
 https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
 
+// 2. Palindrome Linked List
+https://www.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1
+
+
+
 public class PracticeQuestion {
+    public class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String args[]) {
+
+    }
     
 }
