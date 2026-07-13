9ad50fa44d4d53517237d65591cc231cfa2ab9fc
Prabhat kumar <prabhat844502@gmail.com>
2026-07-13 22:04:29 +0530

    insertion of element at bottom of a stack

--- Stack/PracticeQuestion.java
+++ Stack/PracticeQuestion.java
@@ -1,3 +1,8 @@
+// 1. Insert an Element at the Bottom of a Stack
+https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stack/1
+
+
+
 public class PracticeQuestion {
     public static void main(String args[]) {
         
