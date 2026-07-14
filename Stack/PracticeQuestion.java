524164259522390603299bb37bd876ab7c4e6faf
Prabhat kumar <prabhat844502@gmail.com>
2026-07-14 22:30:34 +0530

    Queue Reversal

--- Stack/PracticeQuestion.java
+++ Stack/PracticeQuestion.java
@@ -4,6 +4,8 @@ https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stac
 // 2. Reverse first K of a Queue
 https://www.geeksforgeeks.org/problems/reverse-first-k-elements-of-queue/1
 
+// 3. Queue Reversal
+https://www.geeksforgeeks.org/problems/queue-reversal/1
 
 
 public class PracticeQuestion {
