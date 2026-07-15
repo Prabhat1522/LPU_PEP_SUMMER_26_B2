70638b4aabf7251812d6b3462dab4ed2ee97a1dd
Prabhat kumar <prabhat844502@gmail.com>
2026-07-15 23:19:51 +0530

    Updated stack practice questions

--- Stack/PracticeQuestion.java
+++ Stack/PracticeQuestion.java
@@ -7,7 +7,7 @@ https://www.geeksforgeeks.org/problems/reverse-first-k-elements-of-queue/1
 // 3. Queue Reversal
 https://www.geeksforgeeks.org/problems/queue-reversal/1
 
-// 4. 155. Min Stack
+// 4. 155. Min Stackr
 https://leetcode.com/problems/min-stack/description/
 
 // 5. 225. Implement Stack using Queues
@@ -18,6 +18,23 @@ https://www.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1
 
 // 7. Interleave the First Half of the Queue with Second Half
 https://www.geeksforgeeks.org/problems/interleave-the-first-half-of-the-queue-with-second-half/1
+
+// 8. Next Greater Element
+https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+// 9. Previous Smaller Element
+https://www.geeksforgeeks.org/problems/previous-smaller-element/1
+
+
+// 10 . 84. Largest Rectangle in Histogram
+https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+
+// 11 . 735. Asteroid Collision
+https://leetcode.com/problems/asteroid-collision/
+
+// 12. 622. Design Circular Queue
+https://leetcode.com/problems/design-circular-queue/description/
+
 
 
 public class PracticeQuestion {
